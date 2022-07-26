f28af969cc28f491740f4a8c03b0477a2c6adc1f
Ángela Herrador <angelaherrador1991@gmail.com>
2022-07-26 17:00:57 +0200

    feature: add minorities changes2 git

--- src/services/BaseService.ts
+++ src/services/BaseService.ts
@@ -3,7 +3,7 @@ import { getAccessToken, isTokenValid, logout } from "../store/AccessTokenStore"
 
 export const create = (opts: any): any => {
   const http = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://localhost:8080/api",
     ...opts,
   });
 
