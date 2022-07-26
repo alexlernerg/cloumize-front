0c1ec3bcc6eefbaea52c055d668432f0eb4d7a61
Ángela Herrador <angelaherrador1991@gmail.com>
2022-07-26 10:51:49 +0200

    feature: create Error view with responsive design

--- src/components/Misc/Error/Error.template.tsx
+++ src/components/Misc/Error/Error.template.tsx
@@ -3,7 +3,10 @@ import './Error.scss';
 
 const Error = () => {
   return <div className='ErrorContainer'>
-    <button>Error</button>
+    <img className='ErrorContainer__image' src='./Misc/Error/Error.svg' alt='Error'/>
+    <h6 className='ErrorContainer__title'>We're having trouble connecting with you. </h6>
+    <p className='ErrorContainer__subtitle'>Visit us on our social networks!</p>
+    <button className='ErrorContainer__button'>Visit us at our Linkedin</button>
   </div>;
 };
 
