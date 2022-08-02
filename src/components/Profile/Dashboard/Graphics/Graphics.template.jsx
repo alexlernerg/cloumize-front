c6a51727d08fb2d07fcae34dd74eccd148a8bb96
Ángela Herrador <angelaherrador1991@gmail.com>
2022-08-02 12:41:23 +0200

    feature: vertical and Doughnut graphic done

--- src/components/Profile/Dashboard/Graphics/Graphics.template.jsx
+++ src/components/Profile/Dashboard/Graphics/Graphics.template.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Bar, Doughnut, Pie } from 'react-chartjs-2';
 import './Graphics.scss';
 
@@ -6,28 +6,172 @@ import Chart from 'chart.js/auto';
 import { CategoryScale } from 'chart.js';
 Chart.register(CategoryScale);
 
-const stateVB = {
-  labels: ['January', 'February', 'March', 'April', 'May'],
-  datasets: [
-    {
-      label: 'Rainfall',
-      backgroundColor: 'rgba(75,192,192,1)',
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: [65, 59, 80, 81, 56],
-    },
-  ],
+const dataVerticalBar = [
+  {
+    user_id_cm: 1,
+    period_start: '2021-07-31 19:00:00',
+    period_end: '2021-08-31 19:00:00',
+    spending_on_demand_instances: 1322.12518624,
+    spending_std_reserved_instances: 0.0,
+  },
+  {
+    user_id_cm: 1,
+    period_start: '2021-08-31 19:00:00',
+    period_end: '2021-09-30 19:00:00',
+    spending_on_demand_instances: 1319.0965546592,
+    spending_std_reserved_instances: 0.0,
+  },
+  {
+    user_id_cm: 1,
+    period_start: '2021-09-30 19:00:00',
+    period_end: '2021-10-31 19:00:00',
+    spending_on_demand_instances: 1332.2767557696,
+    spending_std_reserved_instances: 0.0,
+  },
+  {
+    user_id_cm: 1,
+    period_start: '2021-10-31 19:00:00',
+    period_end: '2021-11-30 19:00:00',
+    spending_on_demand_instances: 1172.7441292616,
+    spending_std_reserved_instances: 0.0,
+  },
+  {
+    user_id_cm: 1,
+    period_start: '2021-11-30 19:00:00',
+    period_end: '2021-12-31 19:00:00',
+    spending_on_demand_instances: 1315.966794496,
+    spending_std_reserved_instances: 0.0,
+  },
+  {
+    user_id_cm: 1,
+    period_start: '2021-12-31 19:00:00',
+    period_end: '2022-01-31 19:00:00',
+    spending_on_demand_instances: 1319.9267891176,
+    spending_std_reserved_instances: 0.0,
+  },
+  {
+    user_id_cm: 1,
+    period_start: '2022-01-31 19:00:00',
+    period_end: '2022-02-28 19:00:00',
+    spending_on_demand_instances: 1185.717794468,
+    spending_std_reserved_instances: 0.0,
+  },
+  {
+    user_id_cm: 1,
+    period_start: '2022-02-28 19:00:00',
+    period_end: '2022-03-31 19:00:00',
+    spending_on_demand_instances: 1319.070034856,
+    spending_std_reserved_instances: 0.0,
+  },
+  {
+    user_id_cm: 1,
+    period_start: '2022-03-31 19:00:00',
+    period_end: '2022-04-30 19:00:00',
+    spending_on_demand_instances: 1290.5424247182,
+    spending_std_reserved_instances: 0.0,
+  },
+  {
+    user_id_cm: 1,
+    period_start: '2022-04-30 19:00:00',
+    period_end: '2022-05-31 19:00:00',
+    spending_on_demand_instances: 1320.0750210848,
+    spending_std_reserved_instances: 0.0,
+  },
+  {
+    user_id_cm: 1,
+    period_start: '2022-05-31 19:00:00',
+    period_end: '2022-06-30 19:00:00',
+    spending_on_demand_instances: 0.0,
+    spending_std_reserved_instances: 0.0,
+  },
+  {
+    user_id_cm: 1,
+    period_start: '2022-06-30 19:00:00',
+    period_end: '2022-07-31 19:00:00',
+    spending_on_demand_instances: 574.7957773408,
+    spending_std_reserved_instances: 0.0,
+  },
+];
+
+export const VerticalBar = () => {
+  const labelsVB = dataVerticalBar?.map(
+    (data) => data.period_start.split(' ')[0]
+  );
+  const dataVB = dataVerticalBar.map(
+    (data) => data.spending_on_demand_instances
+  );
+  const dataVB2 = dataVerticalBar.map(
+    (data) => data.spending_std_reserved_instances
+  );
+  const [data, setData] = useState({
+    labels: labelsVB,
+    datasets: [
+      {
+        label: 'On-demand',
+        data: dataVB,
+        backgroundColor: '#80A7FA',
+        hoverBackgroundColor: '#2C6CF6',
+        borderRadius: 5,
+        borderSkipped: false,
+        pointStyle: 'circle',
+      },
+      {
+        label: 'Autopilot',
+        data: dataVB2,
+        backgroundColor: '#E9F0FE',
+        borderRadius: 5,
+        borderSkipped: false,
+        pointStyle: 'circle',
+      },
+    ],
+  });
+  return (
+    <div className='VerticalBar'>
+      <p>Instance Family</p>
+      <Bar
+        data={data}
+        options={{
+          responsive: true,
+          maintainAspectRatio: true,
+          scales: {
+            x: {
+              stacked: true,
+              grid: {
+                display: false,
+              },
+            },
+            y: {
+              stacked: true,
+              max: 15000,
+              ticks: {
+                stepSize: 5000,
+              },
+            },
+          },
+          plugins: {
+            legend: {
+              display: true,
+              position: 'top',
+              labels: {
+                usePointStyle: true,
+              },
+            },
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 const stateD = {
   maintainAspectRatio: false,
   responsive: false,
-  labels: ['a', 'b', 'c', 'd'],
   datasets: [
     {
-      data: [300, 50, 100, 50],
-      backgroundColor: 'rgba(75,192,192,1)',
-      hoverBackgroundColor: 'rgba(0,0,0,1)',
+      label: 'Restless',
+      data: [300],
+      backgroundColor: '#80A7FA',
+      hoverBackgroundColor: '#2C6CF6',
     },
   ],
 };
@@ -48,43 +192,32 @@ const stateHB = {
       data: [65, 59, 80, 81, 56],
     },
   ],
-}
-
-export const VerticalBar = () => {
-  return (
-    <div className='VerticalBar'>
-      <Bar
-        data={stateVB}
-        options={{
-          plugins: {
-            title: {
-              display: true,
-              text: 'Cryptocurrency prices',
-            },
-            legend: {
-              display: true,
-              position: 'bottom',
-            },
-          },
-        }}
-      />
-    </div>
-  );
 };
 
 export const DoughnutGraphic = () => {
+  const [data, setData] = useState({
+    maintainAspectRatio: false,
+    responsive: false,
+    labels: ['Restless'],
+    datasets: [
+      {
+        label: 'Restless',
+        data: [15, 100],
+        backgroundColor: ['#80A7FA','#E9F0FE'],
+        hoverBackgroundColor: ['#2C6CF6','#E9F0FE']
+      },
+    ],
+  });
   return (
     <div className='Doughnut'>
+      <p>Text</p>
       <Doughnut
-        data={stateD}
+        data={data}
         options={{
-          legend: {
-            display: false,
-            position: 'right',
-          },
-          elements: {
-            arc: {
-              borderWidth: 0,
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'bottom',
             },
           },
         }}
