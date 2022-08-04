683e5f28c5abe1472a9e96c511edbf8553c89e88
Ángela Herrador <angelaherrador1991@gmail.com>
2022-08-04 15:05:10 +0200

    feature: delete minorities changes

--- src/components/Profile/Account/Account.template.jsx
+++ src/components/Profile/Account/Account.template.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation, useParams } from "react-router-dom";
 import { createPaymentSession, getPaymentSession, getPrice } from '../../../services/PaymentService';
 import './Account.scss';
 
