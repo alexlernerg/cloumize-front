00b9d543cbf0d3b95a76f001425ca10d129535cc
Ángela Herrador <angelaherrador1991@gmail.com>
2022-08-08 16:41:40 +0200

    wip: work on select logic

--- src/components/Profile/Aux/TableSkeleton/TableSkeleton.template.tsx
+++ src/components/Profile/Aux/TableSkeleton/TableSkeleton.template.tsx
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './TableSkeleton.scss';
 
 const TableMobile = ({ dataContent, columnsContent, pathname }: any) => {
-  console.log('props', dataContent, columnsContent, pathname);
   return (
     <>
-      {dataContent.map((data: any, i:any) => (
+      {dataContent.map((data: any, i: any) => (
         <table className='table mobile TableMobile' key={i}>
           {pathname === '/user/savingsFinder' && (
             <>
@@ -59,120 +58,126 @@ const TableMobile = ({ dataContent, columnsContent, pathname }: any) => {
               </tr>
             </>
           )}
-          {pathname === '/user/computeFinder' && <>
-          <tr>
-            <td>{columnsContent[0]}</td>
-            <td>{data.aws_account_id}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[1]}</td>
-            <td>{data.instance_id}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[2]}</td>
-            <td>{data.instance_type}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[3]}</td>
-            <td>{data.region}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[4]}</td>
-            <td>{data.az}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[5]}</td>
-            <td>{data.platform}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[6]}</td>
-            <td>{data.tenancy}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[7]}</td>
-            <td>{data.instance_lifecycle}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[8]}</td>
-            <td>{data.state}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[9]}</td>
-            <td>{data.launch_time}</td>
-          </tr>
-          </>}
-          {pathname === '/user/existingPlans' && <>
-          <tr>
-            <td>{columnsContent[0]}</td>
-            <td>{data.aws_account_id}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[1]}</td>
-            <td>{data.group_reservation_id}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[2]}</td>
-            <td>{data.status}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[3]}</td>
-            <td>{data.description}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[4]}</td>
-            <td>{data.instance_family}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[5]}</td>
-            <td>{data.reservation_count}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[6]}</td>
-            <td>{data.team_remaining}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[7]}</td>
-            <td>{data.discount}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[8]}</td>
-            <td>{data.autosave}</td>
-          </tr>
-          </>}
-          {pathname === '/user/savingsPlans' && <>
-          <tr>
-            <td>{columnsContent[0]}</td>
-            <td>{data.aws_account_id}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[1]}</td>
-            <td>{data.sp_id_aws}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[2]}</td>
-            <td>{data.status}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[3]}</td>
-            <td>{data.sp_type}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[4]}</td>
-            <td>{data.instance_family}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[5]}</td>
-            <td>{data.term_remaining_d}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[6]}</td>
-            <td>{data.commitment}</td>
-          </tr>
-          <tr>
-            <td>{columnsContent[7]}</td>
-            <td>{data.discount}</td>
-          </tr>
-          </>}
+          {pathname === '/user/computeFinder' && (
+            <>
+              <tr>
+                <td>{columnsContent[0]}</td>
+                <td>{data.aws_account_id}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[1]}</td>
+                <td>{data.instance_id}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[2]}</td>
+                <td>{data.instance_type}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[3]}</td>
+                <td>{data.region}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[4]}</td>
+                <td>{data.az}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[5]}</td>
+                <td>{data.platform}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[6]}</td>
+                <td>{data.tenancy}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[7]}</td>
+                <td>{data.instance_lifecycle}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[8]}</td>
+                <td>{data.state}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[9]}</td>
+                <td>{data.launch_time}</td>
+              </tr>
+            </>
+          )}
+          {pathname === '/user/existingPlans' && (
+            <>
+              <tr>
+                <td>{columnsContent[0]}</td>
+                <td>{data.aws_account_id}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[1]}</td>
+                <td>{data.group_reservation_id}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[2]}</td>
+                <td>{data.status}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[3]}</td>
+                <td>{data.description}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[4]}</td>
+                <td>{data.instance_family}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[5]}</td>
+                <td>{data.reservation_count}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[6]}</td>
+                <td>{data.team_remaining}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[7]}</td>
+                <td>{data.discount}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[8]}</td>
+                <td>{data.autosave}</td>
+              </tr>
+            </>
+          )}
+          {pathname === '/user/savingsPlans' && (
+            <>
+              <tr>
+                <td>{columnsContent[0]}</td>
+                <td>{data.aws_account_id}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[1]}</td>
+                <td>{data.sp_id_aws}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[2]}</td>
+                <td>{data.status}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[3]}</td>
+                <td>{data.sp_type}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[4]}</td>
+                <td>{data.instance_family}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[5]}</td>
+                <td>{data.term_remaining_d}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[6]}</td>
+                <td>{data.commitment}</td>
+              </tr>
+              <tr>
+                <td>{columnsContent[7]}</td>
+                <td>{data.discount}</td>
+              </tr>
+            </>
+          )}
         </table>
       ))}
     </>
@@ -192,6 +197,36 @@ const TableSkeleton = (
     const { name, value } = e.target;
     name === 'search' ? setSearch(value) : setFilterChoosed(value);
   };
+
+  const [doCheck, setDoCheck]: [
+    any,
+    React.Dispatch<React.SetStateAction<any>>
+  ] = useState({all: false, rest:[]});
+
+  console.log("doCheck", doCheck);
+
+  const onCheck = (e: React.FormEvent<HTMLInputElement>) => {
+    const {checked, name} = e.currentTarget;
+    if (name === 'all') {
+      console.log("Selecciono todos", checked, name)
+    } else {
+      console.log("Selecciono uno", checked, name)
+    }
+    // setDoCheck();
+  };
+
+  useEffect(()=> {
+    const filteredArray = dataContent.filter((data:any)=> data.state === 'Pending Approval');
+    console.log("filteredArray", filteredArray);
+    const mapArray = filteredArray.map((data:any) => {
+      const obj = {
+        id: data.aws_account_id,
+        check: false
+      }
+      return obj
+    })
+    doCheck.rest = mapArray
+  },[])
 
   useEffect(() => {
     setFilteredData(
@@ -213,8 +248,12 @@ const TableSkeleton = (
           value={filterChoosed}
           className='TableSkeletonContainer__search-select'
         >
-          {Object.keys(dataContent[0]).map((col: any, i:any) => (
-            <option value={col} className='TableSkeletonContainer__search-col' key={i}>
+          {Object.keys(dataContent[0]).map((col: any, i: any) => (
+            <option
+              value={col}
+              className='TableSkeletonContainer__search-col'
+              key={i}
+            >
               {col}
             </option>
           ))}
@@ -238,15 +277,41 @@ const TableSkeleton = (
       <table className='table TableSkeletonContainer desktop'>
         <thead className='TableSkeletonContainer__head'>
           <tr className='TableSkeletonContainer__head-line'>
-            {columnsContent.map((col: any, i: any) => (
-              <td
-                valign='middle'
-                key={i}
-                className='TableSkeletonContainer__head-col'
-              >
-                {col}
-              </td>
-            ))}
+            {pathname === '/user/savingsFinder'
+              ? columnsContent.map((col: any, i: any) => (
+                  <td
+                    valign='middle'
+                    key={i}
+                    className='TableSkeletonContainer__head-col'
+                  >
+                    {col === 'Approval' ? (
+                      <div className='TableSkeletonContainer__head-approve'>
+                        <p>{col}</p>
+                        <label className='custom-radio-checkbox z-10'>
+                          <input
+                            className='custom-radio-checkbox__input z-10'
+                            type='checkbox'
+                            name='all'
+                            checked={doCheck.all}
+                            onChange={(e) => onCheck(e)}
+                          />
+                          <span className='custom-radio-checkbox__show custom-radio-checkbox__show--checkbox z-10'></span>
+                        </label>
+                      </div>
+                    ) : (
+                      col
+                    )}
+                  </td>
+                ))
+              : columnsContent.map((col: any, i: any) => (
+                  <td
+                    valign='middle'
+                    key={i}
+                    className='TableSkeletonContainer__head-col'
+                  >
+                    {col}
+                  </td>
+                ))}
           </tr>
         </thead>
         <tbody className='TableSkeletonContainer__body'>
@@ -305,17 +370,44 @@ const TableSkeleton = (
                   valign='middle'
                   className='TableSkeletonContainer__body-col'
                 >
-                  {content.state === '' && 'Approved'}
-                  {content.state === 'Pending Approval' && 'Pending'}
-                  {content.state === 'Savings Activated' && 'Active'}
+                  {content.state === '' && (
+                    <p className='TableSkeletonContainer__body-textApproved m-0'>
+                      Approved
+                    </p>
+                  )}
+                  {content.state === 'Pending Approval' && (
+                    <p className='TableSkeletonContainer__body-textPending m-0'>
+                      Pending
+                    </p>
+                  )}
+                  {content.state === 'Savings Activated' && (
+                    <p className='TableSkeletonContainer__body-textActive m-0'>
+                      Active
+                    </p>
+                  )}
                 </td>
                 <td
                   valign='middle'
                   className='TableSkeletonContainer__body-col'
                 >
-                  {content.state === '' && 'Approved'}
-                  {content.state === 'Pending Approval' && 'Pending'}
-                  {content.state === 'Savings Activated' && 'Active'}
+                  {content.state === '' ||
+                  content.state === 'Savings Activated' ? (
+                    <div className='d-flex justify-content-center'>
+                      <img src='/Profile/checkbox.svg' alt='checkbox'/>
+
+                    </div>
+                  ) : (
+                    <label className='custom-radio-checkbox z-10 d-flex justify-content-center'>
+                      <input
+                        className='custom-radio-checkbox__input z-10'
+                        type='checkbox'
+                        name={content.aws_account_id}
+                        checked={doCheck?.rest[i]?.check}
+                        onChange={(e) => onCheck(e)}
+                      />
+                      <span className='custom-radio-checkbox__show custom-radio-checkbox__show--checkbox z-10'></span>
+                    </label>
+                  )}
                 </td>
               </tr>
             ))}
