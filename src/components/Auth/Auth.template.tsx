daf5b73c9e8ae028be3e4e924fd92c2936bbd1e6
alexlernerg <80718396+alexlernerg@users.noreply.github.com>
2022-10-06 22:21:08 +0200

    Update Auth.template.tsx

--- src/components/Auth/Auth.template.tsx
+++ src/components/Auth/Auth.template.tsx
@@ -126,7 +126,7 @@ const Auth = (
               }}
             />
             {touched.password && <div className='AuthContainer__form-error'>
-              <p style={{color: (errors.password.lengthMsg === '' && errors.password.uppercaseMsg === '' && errors.password.lowercaseMsg === '') ? '#13D733' : '#FF434E'}}>Password must contain:</p>
+              <p style={{color: (errors.password.lengthMsg === '' && errors.password.uppercaseMsg === '' && errors.password.lowercaseMsg === '') ? '#13D733' : '#FF434E'}}>The password must contain:</p>
                 <ul>
                   {errors.password.lengthMsg === '' ? <li className='AuthContainer__form-error-correct'>v At least 8 characters </li>: <li>x At least 8 characters</li>}
                   {errors.password.uppercaseMsg === '' ? <li className='AuthContainer__form-error-correct'>v 1 uppercase letter</li>: <li>x 1 uppercase letter</li>}
